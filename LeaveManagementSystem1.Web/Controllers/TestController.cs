90836671921d498ca68b72d8becc42f5c3042e35
VanJacobLopez <VanJacobLopez@IBM-PF2RDCEY>
2024-06-19 16:19:10 +0800

    Add project files.

---
+++ LeaveManagementSystem1.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem1.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem1.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Van Jacob the great Developer",
+                DateOfBirth = new DateTime(1998,09,14)
+            };
+            return View(data);
+        }
+    }
+}
---
+++ LeaveManagementSystem1.Web/Models/TestViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagementSystem1.Web.Models
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        
+    }
+}
